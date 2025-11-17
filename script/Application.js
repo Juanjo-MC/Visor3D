6636e031d0abb15419966691266c62d41fd3b91b
Juanjo-MC <63721215+Juanjo-MC@users.noreply.github.com>
2025-11-17 16:53:26 +0100

    Add files via upload

--- script/Application.js
+++ script/Application.js
@@ -376,6 +376,10 @@ export class Application{
 	}
 
 	static #onCompassDoubleClick(){
+		if ('vibrate' in navigator){
+			navigator.vibrate(100);
+		}
+		
 		const currentCameraPosition = ViewerService.getCameraPosition();
 		const currentHeading = Math.ceil(currentCameraPosition.heading);
 		const newHeading = (currentHeading - (currentHeading % 90) + 90) % 360;
